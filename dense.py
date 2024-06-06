7250ef3ff9792e8aeb5746dd1e97143001c98af2
nebiyu youhannes <nebiyuyouhannes08@gmail.com>
2024-06-06 10:58:27 +0300

    added backpropagation

--- dense.py
+++ dense.py
@@ -5,9 +5,16 @@ class Dense:
     this is the structure of a single hidden layer that is meant to be customized
     """
     def __init__(self,n_inputs,n_neurons):
+        self.inputs=n_inputs
         self.weights=0.01*np.random.randn(n_inputs,n_neurons)
         self.bias=np.zeros((1,n_neurons))
+    
     def forward(self,inputs):
         """ this is the feedforward opertaion of a neural network."""
         self.outputs=np.dot(inputs,self.weights)+self.bias
         return self.outputs
+    
+    def backward(self,dvalues):
+        dinputs=np.dot(self.dvalues,self.weights.T)
+        dweights=np.dot(self.inputs.T,dvalues)
+        dbias=np.sum(dvalues,axis=0,keepdims=True)
