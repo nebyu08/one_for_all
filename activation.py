500360cd38d4bd631e61adab519c53ad223ebda6
nebiyu youhannes <nebiyuyouhannes08@gmail.com>
2024-06-06 21:49:59 +0300

    added backprop

--- activation.py
+++ activation.py
@@ -18,4 +18,13 @@ class Softmax:
         self.scaled=np.exp(inputs - np.max(inputs,axis=1,keepdims=True))
         #lets get probability distribution
         self.outputs=self.scaled/np.sum(self.scaled,axis=1,keepdims=True)
-    
+
+    def backward(self,dvalues):
+        self.dinputs=np.zeros_like(dvalues)
+
+        #lets loop over the matrix of inputs and operate on the vector values
+        for index,(single_outputs,single_dvalues) in enumerate(zip(self.outputs,dvalues)):
+            single_outputs=single_outputs.reshape(-1,1)  #change it into column vector
+            jackobian_matrix=np.diagflat(single_outputs) - np.dot(single_outputs,single_outputs.T)
+            self.dinputs[index]=np.dot(single_outputs,jackobian_matrix)
+        
