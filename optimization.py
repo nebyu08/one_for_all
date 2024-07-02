27bc6f43f34f3d0d42417e0e8643c2faead0c63f
nebiyu youhannes <nebiyuyouhannes08@gmail.com>
2024-07-02 11:38:24 +0300

    added a learning rate update

--- optimization.py
+++ optimization.py
@@ -1,8 +1,21 @@
 import numpy as np
 
 class SGD:
-    def __init__(self,lr=0.01) -> None:
+    def __init__(self,lr=0.01,decay_rate=0) -> None:
         self.learning_rate=lr
+        self.iterations=0
+        self.decay_rate=decay_rate
+    
+
+    def preupdate_params(self):
+        if self.decay_rate:
+            self.learning_rate=self.learning_rate/(1+self.decay_rate*self.iterations)
+    
+    
     def update_params(self,layer):
         layer.weights+=self.learning_rate*layer.dweights
         layer.bias+=self.learning_rate*layer.dbias
+    
+    def postupdate_params(self):
+        self.iterations+=1
+        
