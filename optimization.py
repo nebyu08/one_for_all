8d38873478bfa0d0edffdb34cb5aba5d0e35826c
nebiyu youhannes <nebiyuyouhannes08@gmail.com>
2024-07-02 11:56:21 +0300

    made it more stable

--- optimization.py
+++ optimization.py
@@ -5,17 +5,18 @@ class SGD:
         self.learning_rate=lr
         self.iterations=0
         self.decay_rate=decay_rate
+        self.current_learning_rate=lr
     
 
     def preupdate_params(self):
         if self.decay_rate:
-            self.learning_rate=self.learning_rate/(1+self.decay_rate*self.iterations)
+            self.current_learning_rate=self.learning_rate/(1+self.decay_rate*self.iterations)
     
     
     def update_params(self,layer):
-        layer.weights+=self.learning_rate*layer.dweights
-        layer.bias+=self.learning_rate*layer.dbias
+        layer.weights+=self.current_learning_rate*layer.dweights
+        layer.bias+=self.current_learning_rate*layer.dbias
     
     def postupdate_params(self):
         self.iterations+=1
-        
+
